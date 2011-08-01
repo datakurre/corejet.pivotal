5470f9635298883be1749b75ed2d36478425b73c
Asko Soukka <asko.soukka@iki.fi>
2011-08-01 15:51:52 +0300

    Renamed README for github RestructuredText support.
    --HG--
    rename : README.txt => README.rst

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = "1.0"
 setup(name="corejet.pivotal",
       version=version,
       description="Adds 'pivotal' repository source for corejet.testrunner.",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open("HISTORY.txt").read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
