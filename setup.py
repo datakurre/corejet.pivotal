df321f3a4e97f8c0b01b94f679bedb92db0e9e9e
Asko Soukka <asko.soukka@iki.fi>
2012-09-11 15:09:08 +0300

    PEP8

--- setup.py
+++ setup.py
@@ -1,38 +1,37 @@
 from setuptools import setup, find_packages
 
-version = "1.0.0"
-
-setup(name="corejet.pivotal",
-      version=version,
-      description="Pivotal Tracker data source for corejet.testrunner",
-      long_description=open("README.rst").read() + "\n" +
-                       open("HISTORY.txt").read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name="corejet.pivotal",
+    version="1.0.1",
+    description="Pivotal Tracker data source for corejet.testrunner",
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.txt").read()),
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Programming Language :: Python",
-        ],
-      keywords="corejet pivotal",
-      author="Asko Soukka",
-      author_email="asko.soukka@iki.fi",
-      url="https://github.com/datakurre/corejet.pivotal/",
-      license="GPL",
-      packages=find_packages(exclude=["ez_setup"]),
-      namespace_packages=["corejet"],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          "setuptools",
-          # -*- Other dependencies: -*-
-          "corejet.core",
-          "argparse",
-          "pivotal-py",
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      [corejet.repositorysource]
-      pivotal = corejet.pivotal:pivotalSource
-      [console_scripts]
-      pivotal = corejet.pivotal.tool:__main__
-      """,
-      )
+    ],
+    keywords="corejet pivotal",
+    author="Asko Soukka",
+    author_email="asko.soukka@iki.fi",
+    url="https://github.com/datakurre/corejet.pivotal/",
+    license="GPL",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["corejet"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        # -*- Other dependencies: -*-
+        "corejet.core",
+        "argparse",
+        "pivotal-py",
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    [corejet.repositorysource]
+    pivotal = corejet.pivotal:pivotalSource
+    [console_scripts]
+    pivotal = corejet.pivotal.tool:__main__
+    """,
+)
